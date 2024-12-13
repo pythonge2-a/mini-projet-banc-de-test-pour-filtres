30547ec5238352aa2e43da3797d44968a1b17408
NicolasZaco <nicolas.zaco@heig-vd.ch>
2024-12-13 09:19:10 +0100

    avencement intérface graphique

--- testbench/interface/usr_Interface.py
+++ testbench/interface/usr_Interface.py
@@ -1,48 +1,99 @@
-# fichier de gestion de l'inteface utilisateur
+# fichier de gestion de l'interface utilisateur
 
 import wx
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title='FilterAnalyzer', size=(400, 300))
+        super().__init__(parent=None, title='FilterAnalyzer', size=(400, 400))
         
         # Création d'un panneau
         panel = wx.Panel(self)
-        
-        # texte statique parametrage fréquence
-        text = wx.StaticText(panel, label='Input frequency', pos=(20, 20))
 
-        # zone de texte parametrage fréquence
-        self.text_ctrl = wx.TextCtrl(panel, pos=(150, 20))
-        
-        # texte statique parametrage amplitude
-        text = wx.StaticText(panel, label='Input amplitude', pos=(20, 50))
+        # Titre : Frequency
+        freq_title = wx.StaticText(panel, label='Frequency :', pos=(20, 20))
+        font = freq_title.GetFont()
+        font.SetPointSize(11)
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
+        freq_title.SetFont(font)
 
-        # zone de texte parametrage amplitude
-        self.text_ctrl = wx.TextCtrl(panel, pos=(150, 50))
+        # Texte statique : Min frequency
+        min_freq_label = wx.StaticText(panel, label='Min frequency', pos=(40, 50))
+        self.min_freq_ctrl = wx.TextCtrl(panel, pos=(130, 50), size=(100, -1))
+        min_freq_unit = wx.StaticText(panel, label='[Hz]', pos=(233, 52))   
 
-        # texte statique parametrage forme d'onde
-        text = wx.StaticText(panel, label='Input waveform', pos=(20, 80))
+        # Texte statique : Max frequency
+        max_freq_label = wx.StaticText(panel, label='Max frequency', pos=(40, 80))
+        self.max_freq_ctrl = wx.TextCtrl(panel, pos=(130, 80), size=(100, -1))
+        max_freq_unit = wx.StaticText(panel, label='[Hz]', pos=(233, 82))
 
-        # boutons parametrage forme d'onde
-        self.radio1 = wx.RadioButton(panel, label='Sinus', pos=(150, 80))
-        self.radio2 = wx.RadioButton(panel, label='Square', pos=(150, 100))
-        self.radio3 = wx.RadioButton(panel, label='Triangle', pos=(150, 120))
-        self.radio4 = wx.RadioButton(panel, label='Sawtooth', pos=(150, 140))                       
+        # Titre : Input amplitude
+        input_amp_title = wx.StaticText(panel, label='Input amplitude :', pos=(20, 110))
+        font = input_amp_title.GetFont()
+        font.SetPointSize(11)
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
+        input_amp_title.SetFont(font)
 
-        # Création d'un bouton
-        button = wx.Button(panel, label='Launch test', pos=(150, 200))
-        button.Bind(wx.EVT_BUTTON, self.on_button_click)
-        
+        # Entrée texte amplitude
+        self.amplitude_ctrl = wx.TextCtrl(panel, pos=(130, 140), size=(100, -1))
+        input_amp_unit = wx.StaticText(panel, label='[V]', pos=(233, 142))
+
+        # ---- Choix entre USB ou Ethernet ----
+        # Titre : Connection
+        connection_title = wx.StaticText(panel, label='Connection :', pos=(20, 170))
+        font = connection_title.GetFont()
+        font.SetPointSize(11)
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
+        connection_title.SetFont(font)
+
+        # Choix entre USB ou Ethernet (radio button)
+        self.radio_box = wx.RadioBox(panel, label='USB/Eth.', pos=(130, 190), choices=['USB', 'Ethernet'], majorDimension=1, style=wx.RA_SPECIFY_COLS)
+        self.radio_box.Bind(wx.EVT_RADIOBOX, self.on_radio_selection)
+
+        # Section USB Configuration
+        self.usb_label = wx.StaticText(panel, label='USB ID:', pos=(40, 250))
+        self.usb_ctrl = wx.TextCtrl(panel, pos=(130, 248), size=(100, -1))
+
+        # Section Ethernet Configuration
+        self.ip_label = wx.StaticText(panel, label='IP Address:', pos=(40, 250))
+        self.ip_ctrl = wx.TextCtrl(panel, pos=(130, 248), size=(100, -1))
+
+        self.port_label = wx.StaticText(panel, label='Port:', pos=(40, 280))
+        self.port_ctrl = wx.TextCtrl(panel, pos=(130, 278), size=(100, -1))
+
+        # Cacher les champs au démarrage
+        self.show_usb_fields()
+
         # Affichage de la fenêtre
         self.Show()
 
-    def on_button_click(self, event):
-        wx.MessageBox('Le bouton a été cliqué !', 'Information', wx.OK | wx.ICON_INFORMATION)
+    def on_radio_selection(self, event):
+        selection = self.radio_box.GetStringSelection()
+        if selection == 'USB':
+            self.show_usb_fields()
+        elif selection == 'Ethernet':
+            self.show_ethernet_fields()
+
+    def show_usb_fields(self):
+        # Afficher les champs USB et cacher les champs Ethernet
+        self.usb_label.Show()
+        self.usb_ctrl.Show()
+
+        self.ip_label.Hide()
+        self.ip_ctrl.Hide()
+        self.port_label.Hide()
+        self.port_ctrl.Hide()
+
+    def show_ethernet_fields(self):
+        # Afficher les champs Ethernet et cacher les champs USB
+        self.usb_label.Hide()
+        self.usb_ctrl.Hide()
+
+        self.ip_label.Show()
+        self.ip_ctrl.Show()
+        self.port_label.Show()
+        self.port_ctrl.Show()
 
 if __name__ == '__main__':
     app = wx.App(False)  # L'objet principal de l'application
     frame = MainFrame()  # Création de la fenêtre principale
     app.MainLoop()       # Lancement de la boucle principale
-
-
