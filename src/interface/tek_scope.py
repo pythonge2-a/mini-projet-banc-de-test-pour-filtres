9e732f0bf50dd96732775c67149abd5e3c5b1049
Rynerd <martin.luthi@heig-vd.ch>
2024-12-20 11:04:30 +0100

    add pkpk measure

--- src/interface/tek_scope.py
+++ src/interface/tek_scope.py
@@ -16,6 +16,27 @@ try:
     # Default setup
     scope.write('*RST')
     scope.write('AUTOSCALE')
+
+    # Set automatique mesure the pk-pk value to Ch1 and Ch2
+    scope.write('MEASUrement:MEAS1:SOURCE CH1')
+    scope.write('MEASUrement:MEAS1:TYPe PK2pk')
+    scope.write('MEASUrement:MEAS1:STATE ON')
+    scope.write('MEASUrement:MEAS2:SOURCE CH1')
+    scope.write('MEASUrement:MEAS2:TYPe PK2pk')
+    scope.write('MEASUrement:MEAS2:STATE ON')
+    
+    # Get the pk-pk value of Ch1 and Ch2
+    pik1 = scope.query('MEASUrement:MEAS1:VALue?')
+    pik2 = scope.query('MEASUrement:MEAS2:VALue?')
+
+    # print pik
+    print(pik1)
+    print(pik2)
+
+    # print pik
+    print(scope.query('MEASUrement:MEAS1:VALue?'))
+
+
 except pyvisa.VisaIOError as e:
     print(f"Failed to connect to the oscilloscope: {e}")
 except Exception as e:
